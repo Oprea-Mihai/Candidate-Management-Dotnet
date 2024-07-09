9213b73860dd5b9dd43c520d11f2c8d2e2b00834
Oprea-Mihai <mihai_lucian_oprea@yahoo.com>
2024-07-09 23:23:56 +0200

    Generated the initial migration

---
+++ CandidateManagement/CandidateManagement.Repository/Migrations/20240709212202_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CandidateManagement.Repository.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CallAvailabilityStart = table.Column<TimeSpan>(type: "time", nullable: true),
+                    CallAvailabilityEnd = table.Column<TimeSpan>(type: "time", nullable: true),
+                    LinkedInUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GitHubUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FreeTextComment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
