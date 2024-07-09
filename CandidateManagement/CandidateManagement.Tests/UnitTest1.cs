016c28d95fe7982766b26dcebd96b8de6a6a1701
Oprea-Mihai <mihai_lucian_oprea@yahoo.com>
2024-07-09 23:41:22 +0200

    Added test project

---
+++ CandidateManagement/CandidateManagement.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CandidateManagement.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
