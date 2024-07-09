395cfbc71ce56c10bc37f211c9465c35a361dc58
Oprea-Mihai <mihai_lucian_oprea@yahoo.com>
2024-07-09 23:21:00 +0200

    Created the candidate entity and set dependency injection for AppDbContext

---
+++ CandidateManagement/CandidateManagement.Repository/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CandidateManagement.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateManagement.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Candidate> Candidates { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ CandidateManagement/CandidateManagement.Repository/Entities/Candidate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateManagement.Repository.Entities
+{
+    public class Candidate
+    {
+        [Key]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public TimeSpan? CallAvailabilityStart { get; set; }
+
+        public TimeSpan? CallAvailabilityEnd { get; set; }
+
+        public string? LinkedInUrl { get; set; }
+
+        public string? GitHubUrl { get; set; }
+
+        [Required]
+        public string FreeTextComment { get; set; }
+    }
+}
