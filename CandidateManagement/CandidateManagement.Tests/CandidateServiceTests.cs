1561cbe5a8ea3d282725465fabc0c75d9ef80877
Oprea-Mihai <mihai_lucian_oprea@yahoo.com>
2024-07-10 18:23:22 +0200

    Testing CandidateService

---
+++ CandidateManagement/CandidateManagement.Tests/CandidateServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using CandidateManagement.Repository.Entities;
+using CandidateManagement.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateManagement.Tests
+{
+
+    [TestFixture]
+    public class CandidateServiceTests
+    {
+       
+    }
+}
