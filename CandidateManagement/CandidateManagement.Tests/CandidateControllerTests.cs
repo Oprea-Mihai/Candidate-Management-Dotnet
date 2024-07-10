8157b89b12956611ebf86e0ddd940feb9be4dd79
Oprea-Mihai <mihai_lucian_oprea@yahoo.com>
2024-07-10 20:26:50 +0200

    Added tests for CandidateController

--- CandidateManagement/CandidateManagement.Tests/CandidateControllerTests.cs
+++ CandidateManagement/CandidateManagement.Tests/CandidateControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CandidateManagement.Controllers;
+using CandidateManagement.Repository.Entities;
+using CandidateManagement.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,45 @@ using System.Threading.Tasks;
 
 namespace CandidateManagement.Tests
 {
+    [TestFixture]
     public class CandidateControllerTests
     {
+        private Mock<ICandidateService> _mockCandidateService;
+        private CandidateController _candidateController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCandidateService = new Mock<ICandidateService>();
+            _candidateController = new CandidateController(_mockCandidateService.Object);
+        }
+
+        [Test]
+        public async Task AddOrUpdateCandidate_ModelStateInvalid_ReturnsBadRequest()
+        {
+            // Arrange
+            _candidateController.ModelState.AddModelError("error", "some error");
+
+            // Act
+            var result = await _candidateController.AddOrUpdateCandidate(new Candidate());
+
+            // Assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task AddOrUpdateCandidate_ValidModelState_CallsAddOrUpdateCandidateAsync()
+        {
+            // Arrange
+            var candidate = new Candidate();
+
+            // Act
+            var result = await _candidateController.AddOrUpdateCandidate(candidate);
+
+            // Assert
+            _mockCandidateService.Verify(service => service.AddOrUpdateCandidateAsync(candidate), Times.Once);
+            Assert.IsInstanceOf<OkResult>(result);
+        }
     }
+
 }
